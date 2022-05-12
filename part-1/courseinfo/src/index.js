99660607cfd26f943892e2813187c6421475d491
mooenz <joss92821@hotmail.com>
2022-05-12 18:50:45 -0500

    change main code

--- part-1/courseinfo/src/index.js
+++ part-1/courseinfo/src/index.js
@@ -1,62 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Header = ({ title }) => {
-  return <h1>{title}</h1>;
-};
+const Header = ({ title }) => <h1>{title}</h1>;
 
 const Part = ({ part }) => (
-  <>
-    <p>
-      {part.title} {part.exercises}
-    </p>
-  </>
+  <p>
+    {part.title} {part.exercises}
+  </p>
 );
 
-const Content = ({ content }) => {
-  return (
-    <div>
-      <Part part={content.one} />
-      <Part part={content.two} />
-      <Part part={content.three} />
-    </div>
-  );
-};
+const Content = ({ content }) => (
+  <div>
+    <Part part={content.one} />
+    <Part part={content.two} />
+    <Part part={content.three} />
+  </div>
+);
 
 const Total = ({ total }) => {
-  return <p>Number of exercises: {total}</p>;
+  const totals =
+    total.one.exercises + total.two.exercises + total.three.exercises;
+
+  return <p>Number of exercises: {totals}</p>;
 };
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
-    parts: {
-      one: {
+    parts: [
+      {
         title: 'Fundamentals of React',
         exercises: 10,
       },
-      two: {
+      {
         title: 'Using props to pass data',
         exercises: 7,
       },
-      three: {
+      {
         title: 'State of a component',
         exercises: 14,
       },
-    },
+    ]
   };
 
   return (
     <div>
       <Header title={course.name} />
       <Content content={course.parts} />
-      <Total
-        total={
-          course.parts.one.exercises +
-          course.parts.two.exercises +
-          course.parts.three.exercises
-        }
-      />
+      <Total total={course.parts} />
     </div>
   );
 };
